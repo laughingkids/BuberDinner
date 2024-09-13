942c5262c48fb17cca87eec4e899b2662a98b5c4
laughingkids <developer.wxj@gmail.com>
2024-09-13 15:50:55 +1000

    feat: implement authentication controller and service

--- BuberDinner.Api/Program.cs
+++ BuberDinner.Api/Program.cs
@@ -1,5 +1,11 @@
+using BuberDinner.Application;
+using BuberDinner.Infrastructure;
+
 var builder = WebApplication.CreateBuilder(args);
 {
+    builder.Services
+        .AddApplication()
+        .AddInfrastructure();
     // Add services to the container.
     builder.Services.AddControllers();
     // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
